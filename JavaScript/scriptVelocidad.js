6459ceb3a7d71b2a495c5200ef8f3758d74f98d2
Ezequiel Soriano <ezequielsoriano2004@gmail.com>
2023-10-26 11:31:02 -0300

    peo

--- JavaScript/scriptVelocidad.js
+++ JavaScript/scriptVelocidad.js
@@ -39,17 +39,23 @@ document.getElementById("formEdad").addEventListener("submit", function(event) {
   event.preventDefault();
 
   var edad = parseInt(document.getElementById("edad").value);
+  var img= document.getElementById("imagenn")
 
   if (edad >= 0 && edad <= 5) {
     document.getElementById("edadPuesta").textContent = "Eres un bebé. Por lo tanto no sabes hablar ni caminar y tienes una dependencia absoluta de tus padres. Requieres de adultos para la satisfacción de tus necesidades esenciales."
+    img.src="/assets/images/bebe.jpg"
   } else if (edad >= 6 && edad <= 11) {
     document.getElementById("edadPuesta").textContent = "Eres un niño. Tu desarrollo implica una serie de aprendizajes que serán claves para tu formación como adulto. En tus primeros años de vida, deberás desarrollar tu lenguaje para después aprender a leer y escribir.";
+    img.src="/assets/images/niño.png"
   } else if (edad >= 12 && edad <= 17) {
     document.getElementById("edadPuesta").textContent = "Eres un adolescente. Traes contigo un aumento en la capacidad para el pensamiento abstracto, la visualización del futuro, la empatía y el idealismo. El cuestionamiento de sí mismo y la reproducción.";
+    img.src="/assets/images/adole.png"
   } else if (edad >= 18 && edad <= 59) {
     document.getElementById("edadPuesta").textContent = "Eres adulto. Te caracterizas por la responsabilidad: la capacidad y la determinación para dar la respuesta adecuada a cada una de nuestras circunstancias vitales. No somos responsables de lo que nos sucede, pero sí de la forma en que nos relacionamos con lo que nos sucede.";
+    img.src="/assets/images/adulto.png"
   } else if (edad >= 60 && edad <= 120) {
     document.getElementById("edadPuesta").textContent = "Eres anciano. Empiezas a notar el peso de los años en tu propio cuerpo. Aparecen arrugas, no cuentas con la misma vitalidad y salud de la juventud. Las funciones y capacidades del organismo empiezan a deteriorarse llegada cierta edad. "
+    img.src="/assets/images/anciano.png"
   } else if (edad > 120) {
     document.getElementById("edadPuesta").textContent = "No puedes tener tantos años!!!";
   } else if (isNaN(edad)) {
